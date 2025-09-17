ee8c1010deb49cd571ad7fe246ad1cf26fe188f8
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2025-09-17 13:50:58 +0200

    feat(kyu6): :sparkles: add new kata solution

---
+++ kyu6/takeATenMinuteWalk.py
@@ -0,0 +1,18 @@
+def is_valid_walk(walk):
+    
+    coordinates = [0, 0]
+    onTime = False
+    if len(walk) == 10:
+        onTime = True
+    for direction in walk:
+        if direction == 'n':
+            coordinates[0] += 1
+        elif direction == 'e':
+            coordinates[1] += 1
+        elif direction == 's':
+            coordinates[0] -= 1
+        else:
+            coordinates[1] -= 1
+    if onTime and coordinates == [0, 0]:
+        return True
+    return False
