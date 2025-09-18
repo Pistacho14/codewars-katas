d34fc152c18b6ba179e9a16224e73150f60266a4
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2025-09-18 09:58:45 +0200

    feat(kyu7): :sparkles: add new kata solution

---
+++ kyu7/SumQuadraticNums.py
@@ -0,0 +1,11 @@
+import math
+
+def roots(a,b,c):
+    
+    try:
+        discriminant = math.sqrt(b**2-4*a*c)
+        positive = (-b + math.sqrt(discriminant)) / (2*a)
+        negative = (-b - math.sqrt(discriminant)) / (2*a)
+        return round((positive + negative), 2)
+    except:
+        return None
