a24d88d9b61fa63a3a3780880ee3991a6954c66a
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2025-09-23 10:15:35 +0200

    refactor(kyu7): :art: rename variable name
    renamed a variable that was not respecting python conventions

--- kyu7/printerErrors.py
+++ kyu7/printerErrors.py
@@ -1,7 +1,7 @@
 def printer_error(s):
     total_errors = 0
     missprints = 'nñopqrstuvwxyz'
-    for print in s:
-        if print in missprints:
+    for item in s:
+        if item in missprints:
             total_errors += 1
     return str(total_errors) + '/' + str(len(s))
