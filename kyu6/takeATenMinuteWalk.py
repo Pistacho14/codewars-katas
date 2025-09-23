0b0e957bd9d77ae66536de6e15f705b3ab9c7a6c
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2025-09-23 10:20:42 +0200

    refactor(kyu6): :art: rename variable to align with snake case
    renamed a variable that was typed in cammel case instead of snake

--- kyu6/takeATenMinuteWalk.py
+++ kyu6/takeATenMinuteWalk.py
@@ -1,9 +1,8 @@
 def is_valid_walk(walk):
-    
     coordinates = [0, 0]
-    onTime = False
+    on_time = False
     if len(walk) == 10:
-        onTime = True
+        on_time = True
     for direction in walk:
         if direction == 'n':
             coordinates[0] += 1
@@ -13,6 +12,6 @@ def is_valid_walk(walk):
             coordinates[0] -= 1
         else:
             coordinates[1] -= 1
-    if onTime and coordinates == [0, 0]:
+    if on_time and coordinates == [0, 0]:
         return True
     return False
